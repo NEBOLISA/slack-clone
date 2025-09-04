db3f15c74df6c08ff2a3ac77ba12e549c991c31c
nebolisa25 <kennethnebolisa@gmail.com>
2025-09-04 12:46:46 +0100

    configured the setup codes for all the techs needed for backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import  {ENV}  from './config/env.js';
+import { connectDB } from './config/db.js';
 
 const { MONGO_URI, PORT } = ENV;
 const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 
 
 app.get('/', (req, res) => res.status(200).send('<h1>Hello <br> World</h1>'))
-console.log("mongo uri:", MONGO_URI);
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    connectDB()
+});
