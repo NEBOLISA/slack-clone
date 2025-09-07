e857a7ab22f5278429ef1b6813c73a71c2983451
nebolisa25 <kennethnebolisa@gmail.com>
2025-09-07 12:39:49 +0100

    added the authentication for clerk,inngest and express

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,36 @@
-import express from 'express';
-import  {ENV}  from './config/env.js';
-import { connectDB } from './config/db.js';
-
-const { MONGO_URI, PORT } = ENV;
-const app = express();
+import express from 'express'
+import { ENV } from './config/env.js'
+import { connectDB } from './config/db.js'
+import { clerkMiddleware } from '@clerk/express'
+import { functions, inngest } from './config/inngest.js'
+import { serve } from 'inngest/express'
+const { PORT } = ENV
+const app = express()
 app.use(express.json())
+app.use(clerkMiddleware())
 app.use(express.urlencoded({ extended: true }))
-
-
-
-app.get('/', (req, res) => res.status(200).send('<h1>Hello <br> World</h1>'))
+app.use('/api/inngest', serve({ client: inngest, functions }))
+app.get('/', (req, res) => {
+  console.log(req.auth())
+  res.status(200).send(`<h1>Hello1 <br> World</h1>`)
+})
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-    connectDB()
-});
+  console.log(`Server running on port ${PORT}`)
+  connectDB()
+})
+
+const startServer = async () => {
+  try {
+    await connectDB()
+    if (ENV.NODE_ENV !== 'production') {
+      app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`)
+      })
+    }
+  } catch (error) {
+    console.error('Failed to start server:', error)
+  }
+}
+
+startServer()
+export default app
