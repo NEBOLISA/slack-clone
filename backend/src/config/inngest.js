77da04b7fc1b598dc1a230690aad5fa15382f077
nebolisa25 <kennethnebolisa@gmail.com>
2025-09-08 11:52:16 +0100

    fixed error

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -6,7 +6,7 @@ import { User } from '../models/user.model.js'
 export const inngest = new Inngest({ id: 'my-app' })
 
 const syncUser = inngest.createFunction(
-    { id: 'sync-user' }, {event: 'user.created' },
+    { id: 'sync-user' }, {event: 'clerk/user.created' },
   async ({ event}) => {
       await connectDB()
       const { id, email_addresses, first_name, last_name, image_url } = event.data
