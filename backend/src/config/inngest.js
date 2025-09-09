00f3069b8e60f5e6f1b0668dafcfaa75ed6263e0
nebolisa25 <kennethnebolisa@gmail.com>
2025-09-09 12:36:30 +0100

    after setting up stream in backend

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from 'inngest'
 import { connectDB } from './db.js'
 import { User } from '../models/user.model.js'
+import { deleteStreamUser, upsertStreamUser } from './stream.js'
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: 'slack-clone' })
@@ -18,6 +19,12 @@ const syncUser = inngest.createFunction(
           image: image_url
       }
       await User.create(newUser)
+
+      await upsertStreamUser({
+          id: newUser.clerkId.toString(),
+          name: newUser.name,
+          image: newUser.image,
+      })
     
   }
 )
@@ -27,7 +34,7 @@ const deleteUser = inngest.createFunction({ id: "delete-user-from-db" }, { event
         await connectDB()
         const { id } = event.data
         await User.deleteOne({ clerkId: id })
-  
+  await deleteStreamUser(id.toString())
     })
 
 export const functions = [syncUser,deleteUser]
---
+++ backend/src/config/stream.js
@@ -0,0 +1,29 @@
+import { StreamChat } from 'stream-chat'
+import { ENV } from './env'
+
+
+
+const streamClient = StreamChat.getInstance(ENV.STREAM_API_KEY, ENV.STREAM_API_SECRET)
+
+const upsertStreamUser = async (userData) => { 
+    try {
+        await streamClient.upsertUsers(userData)
+        console.log("User created/updated in Stream", userData.name)
+        return userData
+    } catch (error) {
+        console.error("Error creating/updating user in Stream", error)
+    }
+}
+
+
+const deleteStreamUser = async (userId) => {
+    try {
+        await streamClient.deleteUsers(userId)
+        console.log("User deleted from Stream", userId)
+    } catch (error) {
+        console.error("Error deleting user from Stream", error)
+    }
+}
+
+
+export { streamClient, upsertStreamUser, deleteStreamUser }
