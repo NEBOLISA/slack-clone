9555f15578e9c77c31c9fbd57ad4660009625bbf
nebolisa25 <kennethnebolisa@gmail.com>
2025-09-15 13:03:12 +0100

    edited some css codes

--- frontend/src/components/AuthFeatures.jsx
+++ frontend/src/components/AuthFeatures.jsx
@@ -3,9 +3,9 @@ import { LuMessageCircleMore } from 'react-icons/lu'
 
 const AuthFeatures = ({icon,text}) => {
   return (
-    <div className='flex mt-2 items-center gap-2 px-4 py-3 rounded-xl bg-[#5c6ba8]/30'>
+    <div className='transition-transform ease-in-out hover:translate-x-2 duration-700 flex mt-2 items-center gap-2 px-4 py-3 rounded-xl bg-[#5c6ba8]/30'>
       <div className='bg-white/23 w-7 h-7 rounded-full flex items-center justify-center'>
-              <span>{ icon}</span>
+        <span>{icon}</span>
       </div>
       <span className='text-[12px] font-semibold'>{text}</span>
     </div>
